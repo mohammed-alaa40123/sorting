3e59d6b697e886669e24348fc50b507c82e3d2e1
3amooraz <omarwaleed1907@gmail.com>
2023-12-20 19:48:39 +0200

    done

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
 import numpy as np
 from selection import *
+from insertion import *
 import plotly.express as px
 
 st.title("Bubble Sort Animation with Streamlit")
 st.sidebar.title("Select sorting type")
-sortingOption = st.sidebar.selectbox("Sorting Algoritms", options=["Selection Sort","Bubble Sort", "Quick Sort"])
+sortingOption = st.sidebar.selectbox("Sorting Algoritms", options=["Selection Sort","Bubble Sort", "Quick Sort", "Insertion Sort"])
 amount = st.slider("Select number of elements", min_value=5, max_value=100)
 
 lst = np.random.randint(0, 100, amount)
@@ -14,6 +15,10 @@ x = np.arange(0, amount, 1)
 if sortingOption == "Selection Sort":
     figure,frames=visualise_selection_sort(x,lst)
     show_complexity_selection()
+
+if sortingOption == "Insertion Sort":
+    figure,frames=visualise_insertion_sort(x,lst)
+    show_complexity_insertion()
 
     
 plot_spot = st.empty()
