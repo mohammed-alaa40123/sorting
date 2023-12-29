fdbb88815c10b8cf423b99c3b8e436eafdfdc2d4
3amooraz <omarwaleed1907@gmail.com>
2023-12-29 11:10:29 +0200

    gamed 3

--- codeexplaination.py
+++ codeexplaination.py
@@ -10,13 +10,13 @@ from comb import *
 # Create a mapping between sorting options and the corresponding show_code functions
 sorting_options_mapping = {
     "Selection Sort": (show_selection_code,show_selection_explaination,visualise_selection_sort,show_complexity_selection),
-    "Bubble Sort": (show_bubble_code,show_bubble_explaination,visualise_bubble_sort,show_complexity_selection),
-    # "Insertion Sort": (show_insertion_code,show_insertion_explaination,visualise_insertion_sort,show_complexity_selection),
-    "Merge Sort": (show_merge_code,show_merge_explaination,visualize_mergesort,show_complexity_selection), 
+    "Bubble Sort": (show_bubble_code,show_bubble_explaination,visualise_bubble_sort,show_complexity_bubble),
+    "Insertion Sort": (show_insertion_code,show_insertion_explaination,visualise_insertion_sort,show_complexity_insertion),
+    "Merge Sort": (show_merge_code,show_merge_explaination,visualize_mergesort,show_complexity_merge), 
     # "Heap Sort": (show_heap_code,show_heap_explaination,mainheap,show_complexity_heap),   
     "Bucket Sort": (show_bucket_code,show_bucket_explaination,mainbucket,show_complexity_bucket),
-    "Quick Sort": (show_quick_code,show_quick_explaination,visualise_quick_sort,show_complexity_selection),
-    # "Comb Sort": (show_comb_code,show_comb_explaination,visualise_comb_sort,show_complexity_selection),
+    "Quick Sort": (show_quick_code,show_quick_explaination,visualise_quick_sort,show_complexity_quicksort),
+    "Comb Sort": (show_comb_code,show_comb_explaination,visualise_comb_sort,show_complexity_comb),
 }
 def show_visualisation_and_complexity(sortingOption,x,lst,speed=70):
     figure=frames=None
