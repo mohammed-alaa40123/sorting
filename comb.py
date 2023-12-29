f5f32edb82da9147de2a1a837934fa87e294c72b
3amooraz <omarwaleed1907@gmail.com>
2023-12-29 11:03:08 +0200

    gamed

--- comb.py
+++ comb.py
@@ -42,3 +42,131 @@ data = {
 complexity_df = pd.DataFrame(data)
 def show_complexity_comb():
     st.sidebar.dataframe(complexity_df,hide_index=True)
+
+
+def show_comb_code(lang):
+    if lang == "Python":
+        code = """
+\n size = len(lst)
+
+    gap = size
+
+    swapped = True
+
+    while swapped == True or gap != 1 :
+
+        gap = int((gap * 10)/13)
+        if gap < 1 :
+            gap = 1
+
+        swapped = False
+
+        for i in range(0, size-gap):
+            if lst[i] > lst[i + gap]:
+                lst[i], lst[i + gap]=lst[i + gap], lst[i]
+                swapped = True
+
+    return lst
+    """
+    elif lang == "CPP":
+        code = """
+\n int getNextGap(int gap)
+{
+    // Shrink gap by Shrink factor
+    gap = (gap*10)/13;
+ 
+    if (gap < 1)
+        return 1;
+    return gap;
+}
+ 
+// Function to sort a[0..n-1] using Comb Sort
+void combSort(int a[], int n)
+{
+    int gap = n;
+
+    bool swapped = true;
+ 
+    while (gap != 1 || swapped == true)
+    {
+        gap = getNextGap(gap);
+ 
+        swapped = false;
+ 
+        for (int i=0; i<n-gap; i++)
+        {
+            if (a[i] > a[i+gap])
+            {
+                swap(a[i], a[i+gap]);
+                swapped = true;
+            }
+        }
+    }
+}
+""" 
+
+    else:
+        code = """
+\n function getNextGap(gap)
+    {
+        gap = parseInt((gap*10)/13, 10);
+        if (gap < 1)
+            return 1;
+        return gap;
+    }
+    function sort(arr)
+    {
+        let n = arr.length;
+        let gap = n;
+        let swapped = true;
+
+        while (gap != 1 || swapped == true)
+        {
+            gap = getNextGap(gap);
+  
+            swapped = false;
+
+            for (let i=0; i<n-gap; i++)
+            {
+                if (arr[i] > arr[i+gap])
+                {
+                    let temp = arr[i];
+                    arr[i] = arr[i+gap];
+                    arr[i+gap] = temp;
+                    swapped = true;
+                }
+            }
+        }
+    }
+"""
+    return code
+
+def show_comb_explaination():
+    content =  """<div><article>
+    <p>Comb Sort is an enhancement over Bubble Sort, aiming to address its limitations. While Bubble Sort compares adjacent values, handling one inversion at a time, Comb Sort improves this by employing a gap larger than 1. It begins with a substantial gap size, gradually decreasing it by a factor of 1.3 in each iteration until it reaches 1. Consequently, Comb Sort eliminates multiple inversions with a single swap, leading to improved performance compared to Bubble Sort. This 1.3 shrink factor was determined empirically through testing on more than 200,000 random lists.</p>
+    <h2>What Is a comb Sort Algorithm?</h2>
+    <ul>
+    <li>Initialize Gap and Array: Set an initial gap size, typically starting with the array length. Also, have a boolean flag (swapped) to track swaps.</li>
+    <li>Gap Reduction: Define a shrink factor (commonly 1.3). In each iteration, reduce the gap by multiplying it by the shrink factor. This reduction continues until the gap becomes 1.</li>
+    <li>Iterative Comparison and Swapping: Begin with the initial gap and iterate through the array. Afterthat, compare elements that are gap distance apart and perform a swap if necessary to correct their order.lastly, move through the array in increments of the gap size, making comparisons and swapping elements as needed.</li>
+    <li>Flag to Track Swaps:Utilize a boolean flag (swapped) to note whether any swaps occurred during a pass through the array. If a swap is performed during the comparison phase, set the flag to indicate that a swap occurred.</li>
+    <li>Repeat with Reduced Gap: Continue the iterations with the reduced gap size until the gap becomes 1. At this point, the algorithm performs a final pass with a gap of 1, essentially behaving like a simple Bubble Sort.</li>
+    <li>Overall Execution: Comb Sort combines the benefits of larger gap comparisons with the gradual reduction of the gap size. By employing larger gaps at the start, it targets the removal of multiple inversions with each iteration. As the gap reduces, it refines the array, leading to fewer necessary comparisons and swaps.</li>
+    <li>Termination: The process terminates when the gap becomes 1, and no swaps occur in an iteration, signifying that the array is sorted.</li>
+    </ul>
+    <h2>The Complexity of comb Sort Algorithm</h2>
+    <p>The time complexity of the comb sort algorithm is:</p>
+    <ul>
+    <li>The comb sort algorithm is made up of two nested loops.</li>
+    <li>It has an O (n2) time complexity due to the two nested loops.</li>
+    </ul>
+    <p>Best Case Time Complexity (O(n * log n)): Occurs when the input array is nearly sorted. In this scenario, the larger gap sizes at the start efficiently eliminate multiple inversions, resulting in a time complexity close to O(n * log n)</p>
+    <p>Average Case Time Complexity (O(n^2 / 2^p)): Involves an average-case time complexity of approximately O(n^2 / 2^p), where 'p' represents the number of increments. This scenario accounts for average sorting performance across various inputs and gap reduction factors.</p>
+    <p>Worst Case Time Complexity (O(n^2)): Happens when the input array is in reverse order. Despite the larger gap sizes and reduction factors, the algorithm's performance degrades to a quadratic time complexity of O(n^2) due to the extensive number of necessary comparisons and swaps.</p>
+    <p>Space Complexity: Comb Sort is an in-place sorting algorithm, meaning it sorts the elements within the given array itself.
+    Space Complexity: O(1)
+    The algorithm doesn’t require additional memory proportional to the size of the input; it operates using only a constant amount of extra space, regardless of the array's size.</p>
+    </article>
+    </div>"""
+    st.markdown(content,unsafe_allow_html=True)
+    st.link_button("For More info", "https://www.geeksforgeeks.org/comb-sort/")
--- insertion.py
+++ insertion.py
@@ -33,3 +33,83 @@ data = {
 complexity_df = pd.DataFrame(data)
 def show_complexity_insertion():
     st.sidebar.dataframe(complexity_df,hide_index=True)
+
+
+def show_insertion_code(lang):
+    if lang == "Python":
+        code = """
+\n def insertion_sort (lst):
+    size = len(lst)
+
+    for x in range (1,size):
+        start = lst[x]
+        j = x-1
+
+        while start < lst[j] and j >= 0 :
+            lst[j+1] = lst[j]
+            j -= 1
+        lst[j+1] = start
+
+    return lst
+    """
+    elif lang == "CPP":
+        code = """
+\n void insertionSort(vector<int> &arr) {
+    int n = arr.size();
+    for (int i = 1; i < n; ++i) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+""" 
+
+    else:
+        code = """
+\n public class InsertionSort {
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+"""
+    return code 
+
+def show_insertion_explaination():
+    content =  """<div><article>
+    <p>Indeed, insertion sort operates much like arranging a hand of playing cards.The array gets divided into two segments: a sorted section and an unsorted section.</p>
+    <p>Elements from the unsorted part are chosen and inserted into the appropriate position within the sorted segment.This process continues until all elements are in their correct order. </p>
+    <h2>What Is a insertion Sort Algorithm?</h2>
+    <ul>
+    <li>Begins with the first element considered as the sorted part and the rest as unsorted.</li>
+    <li>Iterates through the unsorted section, picking elements one by one.</li>
+    <li>Each element is compared with the elements in the sorted part and placed in its correct position.</li>
+    <li>Continues until all elements in the unsorted part are incorporated into the sorted section.</li>
+    </ul>
+    <h2>The Complexity of insertion Sort Algorithm</h2>
+    <p>The time complexity of the insertion sort algorithm is:</p>
+    <ul>
+    <li>The insertion sort algorithm is made up of two nested loops.</li>
+    <li>It has an O (n2) time complexity due to the two nested loops.</li>
+    </ul>
+    <p>Best Case Scenario (O(n)): Occurs when the input array is already sorted. In this case, the algorithm only needs to compare each element with its predecessor and perform no swaps because the elements are already in the correct order. The time complexity is linear (O(n)) because it performs n-1 comparisons but no swaps are required.</p>
+    <p>Average Case Scenario (O(n^2)): In an average case, the input array is not perfectly sorted or reversed. The algorithm, for each element in the unsorted portion, compares it with elements in the sorted portion and shifts elements to make space for the current element. This results in approximately n^2/4 comparisons and swaps on average for n elements, leading to a time complexity of O(n^2).</p>
+    <p>Worst Case Scenario (O(n^2)): The worst case arises when the input array is sorted in reverse order. For each element in the unsorted part, it needs to compare and shift elements in the sorted section until it reaches the appropriate position. This results in roughly n^2/2 comparisons and swaps for n elements, leading to a time complexity of O(n^2).</p>
+    <p>Space Complexity: Insertion sort is an in-place sorting algorithm, meaning it requires only a constant amount of additional memory space. Space Complexity: O(1)</p>
+    </article>
+    </div>"""
+    st.markdown(content,unsafe_allow_html=True)
+    st.link_button("For More info", "https://www.geeksforgeeks.org/insertion-sort/")
